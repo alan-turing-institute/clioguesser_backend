b9e1a973dd71a6c0ba89ccccddf7b4b073e36cec
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-09 14:49:46 +0100

    create get_polities_for_year func

--- clioguesser_backend/core/views.py
+++ clioguesser_backend/core/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.contrib.gis.db.models.functions import AsGeoJSON
 from .models import Cliopatria
 
-def cliopatria_list(request):
-    cliopatria_objects = Cliopatria.objects.all().values()
-    return JsonResponse(list(cliopatria_objects), safe=False)
+def get_polities_for_year(displayed_year):
+    """
+    This function returns the polity geoms for a given year for the map.
 
-def cliopatria_detail(request, pk):
-    try:
-        cliopatria_object = Cliopatria.objects.get(pk=pk)
-        return JsonResponse(cliopatria_object)
-    except Cliopatria.DoesNotExist:
-        return JsonResponse({'error': 'Not found'}, status=404)
+    Args:
+        displayed_year (int): The year to display the polities for.
+
+    Returns:
+        dict: The content for the polity shapes.
+    """
+    rows = Cliopatria.objects.filter(
+        polity_start_year__lte=displayed_year, polity_end_year__gte=displayed_year
+    )
+
+    # Convert 'geom' to GeoJSON in the database query
+    rows = rows.annotate(geom_json=AsGeoJSON("geom"))
+    # Filter the rows to return
+    rows = rows.values(
+        # "id",
+        "seshat_id",  # Can use this for querying the Seshat database
+        "name",
+        # "start_year",
+        # "end_year",
+        # "polity_start_year",
+        # "polity_end_year",
+        # "area",
+        "geom_json",
+        # "components",
+        # "member_of",
+        # "wikipedia_name",
+    )
+    shapes = list(rows)
+    content = {
+        "shapes": shapes
+    }
+    return content
