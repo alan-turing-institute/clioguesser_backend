8f82b910c88f2664f7d0bc986f3438efc007ba2b
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2025-06-10 12:40:12 +0100

    Removed github actions in settings.py

--- clioguesser_backend/clioguesser_backend/settings.py
+++ clioguesser_backend/clioguesser_backend/settings.py
@@ -120,18 +120,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GEOGRAPHIC_DB = True
 """GEOGRAPHIC_DB is set to True to enable the geographic database."""
 
-if sys.platform.startswith("darwin"):  # macOS
-    GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
-    GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"
-else:  # linux
-    if os.getenv("GITHUB_ACTIONS") == "true":
-        GDAL_LIBRARY_PATH = "/usr/lib/libgdal.so"
-        GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
-    else:
-        GDAL_LIBRARY_PATH = "/usr/lib/libgdal.so.30"
-        # TODO: find a way to specify this based on the VM: aarch64 or x86_64
-        if sys.platform == "linux":
-            GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
-        else:
-            GEOS_LIBRARY_PATH = '/usr/lib/aarch64-linux-gnu/libgeos_c.so'
-        
+GDAL_LIBRARY_PATH = "/usr/lib/libgdal.so.30"
+# TODO: find a way to specify this based on the VM: aarch64 or x86_64
+if sys.platform == "linux":
+    GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
+else:
+    GEOS_LIBRARY_PATH = '/usr/lib/aarch64-linux-gnu/libgeos_c.so'
